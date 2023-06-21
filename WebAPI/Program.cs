6fb8bec3967d157c5b9710564696c086a78d6394
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-06-21 23:24:00 +0300

    Add Books controller with endpoints, addd BookService with crud methods, add connetctionString json; configure AppDbContext, add BookService in services, configure Swagger UI and SwaggerDoc

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,19 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WebAPI.Data;
+using WebAPI.Data.Models;
+using WebAPI.Data.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+//cofigure the services
+builder.Services.AddTransient<BookService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "my_books_updated_title", Version = "v2" });
+});
 
 var app = builder.Build();
+
+//seed db
+AppDbInitializer.Seed(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v2/swagger.json", "my_books_ui_updated v1"));
 }
 
 app.UseHttpsRedirection();
