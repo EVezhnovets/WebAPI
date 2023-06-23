092294f85310e19b4b1ab4c05483aaba37390bcb
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-06-23 17:39:10 +0300

    Add Author and Publisher services, fix bookService

---
+++ WebAPI/Data/Services/PubisherService.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+using WebAPI.Data.Models;
+using WebAPI.Data.ViewModels;
+using WebAPI.Exceptions;
+
+namespace WebAPI.Data.Services
+{
+    public class PublisherService
+    {
+        private AppDbContext _context;
+        public PublisherService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Publisher AddPublisher(PublisherVM publisher)
+        {
+            if (StringStartsWithNumber(publisher.Name!)) throw new PublisherNameException("Name starts with number", publisher.Name);
+
+            var _publisher = new Publisher()
+            {
+                Name = publisher.Name
+            };
+
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+            return _publisher;
+        }
+
+        public Publisher GetPublisherById(int id) => _context.Publishers.FirstOrDefault(x => x.Id == id)!;
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _pablisherData = _context.Publishers
+                .Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books!.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Title,
+                        BookAuthors = n.Book_Authors.Select(n => n.Author.FullName).ToList()!
+                    }).ToList()
+                })
+                .FirstOrDefault();
+            return _pablisherData!;
+        }
+
+        internal void DeletePublisherById(int id)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+            if (_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception($"The publisher with id: {id} does not exist");
+            }
+        }
+
+        private bool StringStartsWithNumber(string name) => Regex.IsMatch(name, @"^\d");
+    }
+}
