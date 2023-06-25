da50407495005079904d19f803c6d83e38b1d744
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-06-25 16:50:43 +0300

    Config serilog for writening in file and database

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,11 +2,49 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using System.Drawing;
 using WebAPI.Data;
 using WebAPI.Data.Services;
 using WebAPI.Exceptions;
 
+#region serilog logger write to file
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .CreateBootstrapLogger();
+
+//Log.Logger = new LoggerConfiguration()
+//            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+//            .Enrich.FromLogContext()
+//            .WriteTo.Console()
+//            .WriteTo.File("Logs/log.txt",
+//                rollingInterval: RollingInterval.Day,
+//                rollOnFileSizeLimit: true)
+//            .CreateBootstrapLogger();
+#endregion
+
+#region serilog logger write to db
+////write to db
+//Log.Logger = new LoggerConfiguration()
+//    .WriteTo
+//    .MSSqlServer(
+//        connectionString: "Data Source=localhost;Initial Catalog=myBooksDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+//        sinkOptions: new MSSqlServerSinkOptions { TableName = "Logs" })
+//    .CreateLogger();
+#endregion
+
+Log.Information("Hello, world!");
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 // Add services to the container.
 
@@ -17,6 +55,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 builder.Services.AddTransient<BookService>();
 builder.Services.AddTransient<AuthorService>();
 builder.Services.AddTransient<PublisherService>();
+builder.Services.AddTransient<LogsService>();
 
 builder.Services.AddApiVersioning(config =>
 {
@@ -38,6 +77,8 @@ var app = builder.Build();
 
 //seed db
 //AppDbInitializer.Seed(app);
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
